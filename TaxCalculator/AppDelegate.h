06c3efd5475d5f91909daa3356cf5e496f685077
Sha Zhu <Cinn@Shas-MacBook-Pro.local>
2015-03-06 19:48:55 -0800

    Initial Commit

---
+++ TaxCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TaxCalculator
+//
+//  Created by Sha Zhu on 15/3/6.
+//  Copyright (c) 2015年 Cena Entertaiment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TaxCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TaxCalculator
+//
+//  Created by Sha Zhu on 15/3/6.
+//  Copyright (c) 2015年 Cena Entertaiment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
