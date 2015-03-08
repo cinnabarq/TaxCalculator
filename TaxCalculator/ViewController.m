f63a0858013090a74f9d9145ca09c2205f8d9be3
Sha Zhu <Cinn@Shas-MacBook-Pro.local>
2015-03-08 00:10:08 -0800

    version1

--- TaxCalculator/ViewController.m
+++ TaxCalculator/ViewController.m
@@ -10,6 +10,15 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UILabel *resultLabel;
+@property (weak, nonatomic) IBOutlet UITextField *priceTextField;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+@property double caTax;
+@property double chiTax;
+@property double nyTax;
+
+
 @end
 
 @implementation ViewController
@@ -17,6 +26,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.caTax = .075;
+    self.chiTax = .0925;
+    self.nyTax = .045;
+    
+}
+
+
+- (IBAction)onCalculateButtonTapped:(id)sender
+{
+    
+    NSString *enteredText = self.priceTextField.text;
+    double enteredValue = enteredText.intValue;
+    
+    if (self.segmentedControl.selectedSegmentIndex == 0)
+    {
+        double result = enteredValue * self.caTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
+    
+    else if (self.segmentedControl.selectedSegmentIndex == 1)
+    {
+        double result = enteredValue * self.chiTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
+    
+    else
+    {
+        double result = enteredValue * self.nyTax;
+        self.resultLabel.text = [NSString stringWithFormat:@"%.2f", result];
+    }
 }
 
 - (void)didReceiveMemoryWarning {
